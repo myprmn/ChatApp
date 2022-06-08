791fad6458bd9f1f99b866fbaa1296df75f06398
YUAR <yusuppermana19@gmail.com>
2022-06-09 06:29:27 +0700

    Update login.kt sudah bisa

--- app/src/main/java/com/example/chatapp/Login.kt
+++ app/src/main/java/com/example/chatapp/Login.kt
@@ -1,11 +1,47 @@
 package com.example.chatapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class Login : AppCompatActivity() {
+
+    lateinit var auth : FirebaseAuth
+    lateinit var dbSignIn : DatabaseReference
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        auth = FirebaseAuth.getInstance()
+        dbSignIn = FirebaseDatabase.getInstance().reference
+
+        btnSignIn.setOnClickListener {
+            val email = etEmailSignIn.text.toString()
+            val password = etPasswordSignIn.text.toString()
+            if (email !="" && password!=""){
+                DataLogin(email, password)
+            } else {
+                Toast.makeText(this,"Field masih kosong",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun DataLogin(email:String, password: String) {
+        auth.signInWithEmailAndPassword(email,password)
+            .addOnCompleteListener(this){task->
+                if (task.isComplete){
+                    Toast.makeText(this,"Login Berhasil",Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this,MainActivity::class.java))
+                } else {
+                    Toast.makeText(this,"Login Gagal",Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
